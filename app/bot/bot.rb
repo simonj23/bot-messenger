1b8c4f197b07d519774a15ddf6f892b489defcab
Simon Vielpeau <simon@vielpeau.com>
2018-12-27 21:59:29 +0100

    add suscribed fields in bot.rb

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,15 +2,18 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: ENV["ACCESS_TOKEN"],
+  subscribed_fields: %w[feed mention name]
+)
 
 Bot.on :message do |message|
     if message.text.include? "Bonjour"
-       message.reply(text: "Wesh maggl") 
+       message.reply(text: "Wesh maggl")
    elsif message.text.include? "Bonne nuit"
-        message.reply(text: "Dors bien fdp") 
+        message.reply(text: "Dors bien fdp")
     else
         message.reply(text: 'Reponse par defaut')
     end
-  
+
 end
