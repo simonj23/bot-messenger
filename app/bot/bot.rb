a1b1f1cb70c55835e25fcb554fac482b0739f494
Simon Vielpeau <simon@vielpeau.com>
2018-12-27 23:36:07 +0100

    it was a bad idea lol, doesn't work

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -9,11 +9,11 @@ Facebook::Messenger::Subscriptions.subscribe(
 
 Bot.on :message do |message|
 
-    if message.text.downcase.include? "Bonjour"
+    if message.text.include? "Bonjour"
         message.reply(text: "Salut l'humain !")
-    elsif message.text.downcase.include? "Ça va ?"
+    elsif message.text.include? "Ça va ?"
         message.reply(text: "Je suis un robot, je n'ai donc pas d'émotion.")
-    elsif message.text.downcase.include? "Comment t'appelles tu ?"
+    elsif message.text.include? "Comment t'appelles tu ?"
         message.reply(text: 'Mon créateur s\'appelle Simon !')
     else
         message.reply(text: 'Je suis en train d\'apprendre et ne sais pas quoi répondre')
