8b5f922aaec7cd078c4f0593233eeeffa6813ce7
Simon Vielpeau <simon@vielpeau.com>
2018-12-27 23:30:39 +0100

    downcase texts

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -9,11 +9,11 @@ Facebook::Messenger::Subscriptions.subscribe(
 
 Bot.on :message do |message|
 
-    if message.text.include? "Bonjour"
+    if message.text.downcase.include? "Bonjour"
         message.reply(text: "Salut l'humain !")
-    elsif message.text.include? "Ça va ?"
-        message.reply(text: "Je suis un robot, je n'est donc pas d'émotion.")
-    elsif message.text.include? "Comment t'appelles tu ?"
+    elsif message.text.downcase.include?. "Ça va ?"
+        message.reply.downcase.(text: "Je suis un robot, je n'ai donc pas d'émotion.")
+    elsif message.text.downcase.include? "Comment t'appelles tu ?"
         message.reply(text: 'Mon créateur s\'appelle Simon !')
     else
         message.reply(text: 'Je suis en train d\'apprendre et ne sais pas quoi répondre')
