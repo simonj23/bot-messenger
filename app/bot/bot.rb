3c6f78eefb6c8a5d7242abab60dbdb9d019575eb
Simon Vielpeau <simon@vielpeau.com>
2018-12-27 21:48:40 +0100

    first commit

---
+++ app/bot/bot.rb
@@ -0,0 +1,11 @@
+require 'facebook/messenger'
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: ENV['ACCESS_TOKEN'],
+  subscribed_fields: %w[feed mention name]
+)
+
+Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
