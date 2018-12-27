b70a0f1f161dfaea594a866145c884e168777e3a
Simon Vielpeau <simon@vielpeau.com>
2018-12-27 22:13:21 +0100

    remove feed field if it is not nécessaire

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,7 +4,7 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV["ACCESS_TOKEN"],
-  subscribed_fields: %w[feed mention name]
+  subscribed_fields: %w[mention name]
 )
 
 Bot.on :message do |message|
