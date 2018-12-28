5e13441811ebd9d5066a4b2687c2915412d11851
Simon Vielpeau <simon@vielpeau.com>
2018-12-28 09:35:26 +0100

    return to the base lol

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -7,32 +7,16 @@ Facebook::Messenger::Subscriptions.subscribe(
   subscribed_fields: %w[feed mention name]
 )
 
-
 Bot.on :message do |message|
 
-  message.typing_on
-  sleep 1
-  message.typing_off
-
-  # easy
-  if message.text.include? "Bonjour"
-      message.reply(text: "Salut l'humain !")
-  elsif message.text.include? "Ça va ?"
-      message.reply(text: "Je suis un robot, je n'ai donc pas d'émotion.")
-  elsif message.text.include? "Comment t'appelles tu ?"
-      message.reply(text: 'Mon créateur s\'appelle Simon !')
-  else
-
-  end
+    if message.text.include? "Bonjour"
+        message.reply(text: "Salut l'humain !")
+    elsif message.text.include? "Ça va ?"
+        message.reply(text: "Je suis un robot, je n'ai donc pas d'émotion.")
+    elsif message.text.include? "Comment t'appelles tu ?"
+        message.reply(text: 'Mon créateur s\'appelle Simon !')
+    else
+        message.reply(text: 'Je suis en train d\'apprendre et ne sais pas quoi répondre')
+    end
 
 end
-
-Bot.on :postback do |postback|
-  case postback.payload
-  when '1'
-    puts "Tu préfères Messi !"
-  when '2'
-    puts "Tu préfères Ronaldo !"
-  end
-end
-
