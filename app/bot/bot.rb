9e12b4ff130a3b54c6dffddb18e7c9e7036c8fc9
Simon Vielpeau <simon@vielpeau.com>
2018-12-30 12:33:03 +0100

    double || to set <or>

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -11,11 +11,11 @@ Facebook::Messenger::Subscriptions.subscribe(
 Bot.on :message do |message|
 
 
-    if message.text.include? "bonjour" | "hello" | "yo" | "coucou"
+    if message.text.include? "bonjour" || "hello" || "yo" || "coucou"
         message.reply(text: "Salut l'humain !")
     elsif message.text.include? "ça va"
         message.reply(text: "Je suis un robot, je n'ai donc pas d'émotion.")
-    elsif message.text.include? "comment t'appelles tu ?" | "quel est ton nom"
+    elsif message.text.include? "comment t'appelles tu ?" || "quel est ton nom"
         message.reply(text: 'Mon créateur s\'appelle Simon !')
     else
         message.reply(text: 'Ne sais pas quoi répondre')
