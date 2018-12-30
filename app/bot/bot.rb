99ee11db06062b7e384034bc73d54d06e9e1efdc
Simon Vielpeau <simon@vielpeau.com>
2018-12-30 12:19:57 +0100

    disable v-bucks dad

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -15,10 +15,6 @@ Bot.on :message do |message|
         message.reply(text: "Je suis un robot, je n'ai donc pas d'émotion.")
     elsif message.text.include? "Comment t'appelles tu ?"
         message.reply(text: 'Mon créateur s\'appelle Simon !')
-    elsif message.text.include? "Mets-moi 1000 v-bucks sur le hugoviel"
-        message.reply(text: "TENTATIVE DE RECONNEXION AU SERVEUR EN COURS")
-        sleep(1.5)
-        message.reply(text: "LES V-BUCKS ONT ÉTÉ AJOUTÉS AVEC SUCCÈS SUR LE COMPTE HUGOVIEL")
     else
         message.reply(text: 'Je suis en train d\'apprendre et ne sais pas quoi répondre')
     end
