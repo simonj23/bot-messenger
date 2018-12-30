a675334d7a117e951a54a82d4bdd8bcdef4446de
Simon Vielpeau <simon@vielpeau.com>
2018-12-30 12:40:50 +0100

    return to the base

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -13,12 +13,11 @@ end
 
 Bot.on :message do |message|
 
-
-    if message.text.include_any?(["bonjour", "hello", "yo", "coucou"])
+    if message.text.include? "Bonjour"
         message.reply(text: "Salut l'humain !")
-    elsif message.text.include? "ça va"
+    elsif message.text.include? "Ça va"
         message.reply(text: "Je suis un robot, je n'ai donc pas d'émotion.")
-    elsif message.text.include_any?(["comment t'appelles tu ?", "quel est ton nom"])
+    elsif message.text.include? "Comment t'appelles tu ?"
         message.reply(text: 'Mon créateur s\'appelle Simon !')
     else
         message.reply(text: 'Ne sais pas quoi répondre')
