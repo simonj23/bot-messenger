83a897cb8e56356d944c78f76586f37ce25262ff
Simon Vielpeau <simon@vielpeau.com>
2018-12-30 12:30:19 +0100

    disable downcase because don't work

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -7,16 +7,15 @@ Facebook::Messenger::Subscriptions.subscribe(
   subscribed_fields: %w[feed mention name]
 )
 
-puts "test"
 
 Bot.on :message do |message|
 
 
-    if message.text.downcase.include? "bonjour" | "hello" | "yo" | "coucou"
+    if message.text.include? "bonjour" | "hello" | "yo" | "coucou"
         message.reply(text: "Salut l'humain !")
-    elsif message.text.downcase.include? "ça va"
+    elsif message.text.include? "ça va"
         message.reply(text: "Je suis un robot, je n'ai donc pas d'émotion.")
-    elsif message.text.downcase.include? "comment t'appelles tu ?" | "quel est ton nom"
+    elsif message.text.include? "comment t'appelles tu ?" | "quel est ton nom"
         message.reply(text: 'Mon créateur s\'appelle Simon !')
     else
         message.reply(text: 'Ne sais pas quoi répondre')
